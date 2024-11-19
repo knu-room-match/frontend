f8e80387ec9597869075f5e3c551a1eb750acf24
toothlessdev <toothless042@gmail.com>
2024-11-19 20:33:29 +0900

    feature: add navigate

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 import { Button } from "@/components/ui/button";
 
 import bgMain from "@/assets/bg__main.webp";
 
 export default function HomePage() {
+    const navigate = useNavigate();
+
     return (
         <div className="relative w-full max-w-[600px] h-[100dvh] mx-auto">
             <img
@@ -19,8 +23,12 @@ export default function HomePage() {
                     <p className="text-white">경북대학교 룸메이트 매칭 서비스</p>
                 </div>
                 <div className="flex flex-col gap-3 mb-6">
-                    <Button variant="default">로그인</Button>
-                    <Button variant="outline">룸메이트 둘러보기</Button>
+                    <Button variant="default" onClick={() => navigate("/auth/signin")}>
+                        로그인
+                    </Button>
+                    <Button variant="outline" onClick={() => navigate("/roommates")}>
+                        룸메이트 둘러보기
+                    </Button>
                 </div>
             </div>
         </div>
