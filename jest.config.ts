bd911016e597dec53fe30101f6666f84486444f0
toothlessdev <toothless042@gmail.com>
2024-11-20 19:53:21 +0900

    chore: jest, react-testing-libraray setup

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    moduleNameMapper: {
+        "^.+\\.svg$": "jest-svg-transformer",
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    globals: {
+        "ts-jest": {
+            tsconfig: "tsconfig.test.json",
+        },
+    },
+};
+
+export default config;
